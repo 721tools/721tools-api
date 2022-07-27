c20457db60692fdb2e351d2374ab6ca84a3efcf5
veryyoung <veryyoung.me@gmail.com>
2022-07-27 12:33:28 +0800

    Set token rank

--- src/routes/wallets.ts
+++ src/routes/wallets.ts
@@ -6,13 +6,14 @@ import _ from 'underscore';
 import Sequelize from 'sequelize';
 import { RateLimiterMemory, RateLimiterQueue } from 'rate-limiter-flexible';
 
-import { OpenseaCollections } from '../dal/db';
+import { OpenseaCollections, OpenseaItems } from '../dal/db';
 import genericErc20Abi from "../abis/ERC20.json";
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.ETH_RPC_URL);
 const erc20Contract = new ethers.Contract("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", genericErc20Abi, provider);
 
-const WalletsRouter = new Router({})
+const WalletsRouter = new Router({});
+const Op = Sequelize.Op;
 
 const limiterFlexible = new RateLimiterMemory({
   points: 1,
@@ -60,6 +61,56 @@ const fetchNFTs = async (owner: any) => {
   return items;
 }
 
+const setFloorPrice = async (nfts) => {
+  if (nfts && nfts.length > 0) {
+    const slugs = [...new Set(nfts.map(item => item.slug))];
+    const collectionsRes = await OpenseaCollections.findAll({
+      where: {
+        slug: slugs
+      }
+    });
+
+    if (collectionsRes && collectionsRes.length > 0) {
+      const collectionMap = new Map<string, typeof OpenseaCollections>(collectionsRes.map((item: { slug: string; dataValues: any; }) => [item.slug, item.dataValues]));
+      for (let index in nfts) {
+        const nft = nfts[index];
+        if (collectionMap.has(nft.slug)) {
+          const collection = collectionMap.get(nft.slug);
+          nft.floor_price = parseFloat(parseFloat(collection.floor_price).toFixed(4));
+        }
+        nfts[index] = nft;
+      }
+    }
+  }
+  return nfts;
+}
+
+const setRank = async (nfts) => {
+  if (nfts && nfts.length > 0) {
+    const selectTokens = [];
+    for (const nft of nfts) {
+      selectTokens.push({
+        "collection_slug": nft.slug,
+        "token_address": nft.token_id
+      });
+    }
+    const itemsRes = await OpenseaItems.findAll({ where: { [Op.or]: selectTokens } });
+
+    if (itemsRes && itemsRes.length > 0) {
+      const itemMap = new Map<string, typeof OpenseaItems>(itemsRes.map((item: { collection_slug: string; token_address: string; dataValues: any; }) => [item.collection_slug + item.token_address, item.dataValues]));
+      for (let index in nfts) {
+        const nft = nfts[index];
+        if (itemMap.has(nft.slug + nft.token_id)) {
+          const item = itemMap.get(nft.slug + nft.token_id);
+          nft.rank = item.traits_rank;
+        }
+        nfts[index] = nft;
+      }
+    }
+  }
+  return nfts;
+}
+
 
 
 WalletsRouter.get('/:address/assets', async (ctx: { params: { address: any; }; body: { total_value_in_usd?: number; total_value_in_eth?: number; balance?: number; erc20_balances?: { WETH: number; }; nfts?: any[]; params?: { total_value_in_usd: number; total_value_in_eth: number; balance: number; erc20_balances: { WETH: number; }; nfts: any[]; }; }; }) => {
@@ -91,28 +142,11 @@ WalletsRouter.get('/:address/assets', async (ctx: { params: { address: any; }; b
     result.total_value_in_usd = parseFloat((result.total_value_in_eth * ethPrice.data.USD).toFixed(4));
   }
 
-  // todo set and rank
   result.nfts = await fetchNFTs(address);
-  if (result.nfts && result.nfts.length > 0) {
-    const slugs = [...new Set(result.nfts.map(item => item.slug))];
-    console.log(slugs);
-    const collectionsRes = await OpenseaCollections.findAll({
-      where: {
-        slug: slugs
-      }
-    });
 
-    if (collectionsRes && collectionsRes.length > 0) {
-      const collectionMap = new Map<string, typeof OpenseaCollections>(collectionsRes.map((item: { slug: string; dataValues: any; }) => [item.slug, item.dataValues]));
-      for (let index in result.nfts) {
-        const nft = result.nfts[index];
-        if (collectionMap.has(nft.slug)) {
-          const collection = collectionMap.get(result.nfts[index].slug);
-          nft.floor_price = collection.floor_price;
-        }
-      }
-    }
-  }
+  result.nfts = await setFloorPrice(result.nfts);
+  result.nfts = await setRank(result.nfts);
+
 
   ctx.body = {
     params: result
