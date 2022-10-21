6c393086cbfbdc9aa42e0080f4116a01a1fa0e54
veryyoung <veryyoung.me@gmail.com>
2022-10-21 15:32:39 +0800

    Support new table

--- src/dal/models/nft_sales.js
+++ src/dal/models/nft_sales.js
@@ -12,15 +12,29 @@ module.exports = class NFTSales extends Model {
         type: DataTypes.STRING.BINARY,
         allowNull: false,
       },
-      // FIXME: typo from db
+      tx_hash_string: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        defaultValue: ""
+      },
       plateform: {
         type: DataTypes.STRING.BINARY,
       },
       from: {
         type: DataTypes.STRING.BINARY,
       },
+      from_string: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        defaultValue: ""
+      },
       to: {
         type: DataTypes.STRING.BINARY,
+      },
+      to_string: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        defaultValue: ""
       },
       offer_item_type: {
         type: DataTypes.TINYINT,
@@ -28,11 +42,26 @@ module.exports = class NFTSales extends Model {
       offer_token: {
         type: "VARBINARY(32)",
       },
+      offer_token_string: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        defaultValue: ""
+      },
       offer_identifier: {
         type: "VARBINARY(32)",
       },
+      offer_identifier_string: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        defaultValue: ""
+      },
       offer_amount: {
         type: "VARBINARY(32)",
+      },
+      offer_amount_string: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        defaultValue: ""
       },
       fulfiller_item_type: {
         type: DataTypes.TINYINT,
@@ -40,11 +69,26 @@ module.exports = class NFTSales extends Model {
       fulfiller_token: {
         type: "VARBINARY(32)",
       },
+      fulfiller_token_string: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        defaultValue: ""
+      },
       fulfiller_identifier: {
         type: "VARBINARY(32)",
       },
+      fulfiller_identifier_string: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        defaultValue: ""
+      },
       fulfiller_amount: {
         type: "VARBINARY(32)",
+      },
+      fulfiller_amount_string: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        defaultValue: ""
       },
       direction: {
         type: DataTypes.TINYINT,
