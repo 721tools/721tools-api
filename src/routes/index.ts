6370263f953edf50af0d3c3f9cb1753398cff940
veryyoung <veryyoung.me@gmail.com>
2022-12-13 16:24:53 +0800

    Remove not using routers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,11 +10,9 @@ const orders = require('./orders');
 
 const router = new Router();
 
-router.use('/api/listings', listing.routes(), listing.allowedMethods());
 router.use('/api/wallets', wallets.routes(), wallets.allowedMethods());
 router.use('/api/smart-buys', smartBuys.routes(), smartBuys.allowedMethods());
 router.use('/api/collections', collections.routes(), collections.allowedMethods());
-router.use('/api/statistics', statistics.routes(), statistics.allowedMethods());
 router.use('/api/orders', orders.routes(), orders.allowedMethods());
 router.use('/api/auth', auth.routes(), auth.allowedMethods());
 
