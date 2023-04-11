3447855abef5ea1817135a008fefc85e92655884
veryyoung <veryyoung.me@gmail.com>
2023-04-11 14:31:30 +0900

    Change weth address to goerli

--- src/helpers/opensea/erc20_utils.ts
+++ src/helpers/opensea/erc20_utils.ts
@@ -58,6 +58,11 @@ export const estimateTransferERC20 = async (provider, contractAddress, address, 
 };
 
 
+// export const getWethAddress = () => {
+//     return process.env.NETWORK === 'goerli' ? "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6" : "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+// }
+
+// @todo use it as it after test
 export const getWethAddress = () => {
-    return process.env.NETWORK === 'goerli' ? "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6" : "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+    return "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6";
 }
