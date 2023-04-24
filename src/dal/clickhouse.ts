740752d6793f1bc0805ba78bdd9bf36efaaa1991
veryyoung <veryyoung.me@gmail.com>
2023-04-24 14:15:09 +0900

    Remove clickhouse session_id

--- src/dal/clickhouse.ts
+++ src/dal/clickhouse.ts
@@ -15,11 +15,11 @@ const clickhouse = new ClickHouse({
     format: "json",
     raw: false,
     config: {
-        session_id                              : '',
-        session_timeout                         : 60,
-        output_format_json_quote_64bit_integers : 0,
-        enable_http_compression                 : 0,
-        database                                : '721_nft_assets',
+        // session_id                              : '',
+        session_timeout: 60,
+        output_format_json_quote_64bit_integers: 0,
+        enable_http_compression: 0,
+        database: '721_nft_assets',
     },
 });
 
