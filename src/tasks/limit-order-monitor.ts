9dfed51359edc8c55499fd3f15d7e46798de0cb7
veryyoung <veryyoung.me@gmail.com>
2023-05-18 15:00:00 +0900

    Use orderFilleds to update logs

--- src/tasks/limit-order-monitor.ts
+++ src/tasks/limit-order-monitor.ts
@@ -1,13 +1,14 @@
-import Sequelize from 'sequelize';
+import Sequelize, { where } from 'sequelize';
 import { ethers } from "ethers";
 import _ from 'underscore';
 
 
-import { LimitOrders, OrderBuyLogs } from "../dal/db";
+import { LimitOrders, OrderBuyLogs, OrderFilleds } from "../dal/db";
 import { LimitOrderStatus } from '../model/limit-order-status';
 import { BuyStatus } from '../model/buy-status';
 
 async function main(): Promise<void> {
+    const provider = new ethers.providers.JsonRpcProvider(process.env.NETWORK === 'goerli' ? process.env.GOERLI_RPC_URL : process.env.ETH_RPC_URL);
     while (true) {
         const orderBuyLogs = await OrderBuyLogs.findAll({
             where: {
@@ -18,37 +19,73 @@ async function main(): Promise<void> {
             order: [['id', 'ASC']]
         });
 
-        const provider = new ethers.providers.JsonRpcProvider(process.env.NETWORK === 'goerli' ? process.env.GOERLI_RPC_URL : process.env.ETH_RPC_URL);
-        for (const orderBuyLog of orderBuyLogs) {
-            const txReceipt = await provider.getTransactionReceipt(orderBuyLog.tx);
-            if (txReceipt.status == 1) {
-                await orderBuyLog.update({
-                    status: BuyStatus[BuyStatus.SUCCESS]
-                });
-                const limitOrder = await LimitOrders.findOne({
-                    where: {
-                        id: orderBuyLog.order_id
-                    }
-                });
+        const groupedOrderBuyLogs = _.groupBy(orderBuyLogs, function (orderBuyLog) {
+            return orderBuyLog.order_id + "-" + orderBuyLog.tx;
+        });
 
-                // @todo handle one tx buy many
-                if (limitOrder.purchased + 1 == limitOrder.amount) {
+
+        groupedOrderBuyLogs.forEach(async ([orderIdAndTx, logs]) => {
+            const orderId = orderIdAndTx.split("-")[0];
+            const tx = orderIdAndTx.split("-")[1];
+            let currentPurchased = 0;
+
+            const toBequeriedOrders = [];
+            for (const log of logs) {
+                toBequeriedOrders.push({
+                    "address": ethers.utils.getAddress(log.contract_address),
+                    "tokenId": log.token_id,
+                    "tx_hash": log.tx,
+                });
+            }
+            const orderFilleds = await OrderFilleds.findAll({ where: { [Sequelize.Op.or]: toBequeriedOrders } });
+            const groupedOrderFilleds = _.groupBy(orderFilleds, function (orderFilled) {
+                return orderFilled.address + "-" + orderFilled.tokenId + "-" + orderFilled.tx_hash;
+            });
+            for (const log of logs) {
+                const mapKey = ethers.utils.getAddress(log.contract_address) + "-" + log.token_id + "-" + log.tx;
+                if (mapKey in groupedOrderFilleds) {
+                    const currentOrderFilleds = groupedOrderFilleds[mapKey];
+                    for (const orderFilled of currentOrderFilleds) {
+                        currentPurchased += orderFilled.amount;
+                    }
+                }
+            }
+            const limitOrder = await LimitOrders.findOne({
+                where: {
+                    id: orderId
+                }
+            });
+            if (currentPurchased > 0) {
+                if (limitOrder.purchased + currentPurchased == limitOrder.amount) {
                     await limitOrder.update({
                         status: LimitOrderStatus[LimitOrderStatus.FINISHED],
-                        purchased: limitOrder.purchased + 1
+                        purchased: limitOrder.purchased + currentPurchased
                     });
                 } else {
                     await limitOrder.update({
-                        purchased: limitOrder.purchased + 1
+                        purchased: limitOrder.purchased + currentPurchased
                     });
                 }
             } else {
-                await orderBuyLog.update({
-                    status: BuyStatus[BuyStatus.FAILED]
-                });
+                const txReceipt = await provider.getTransactionReceipt(tx);
+                if (txReceipt.status == 0) {
+                    await limitOrder.update({
+                        status: LimitOrderStatus[LimitOrderStatus.FINISHED],
+                    });
+
+                    await OrderBuyLogs.update({
+                        status: BuyStatus[BuyStatus.FAILED]
+                    }, {
+                        where: {
+                            order_id: orderId,
+                            tx: tx,
+                        }
+                    });
+                }
+
             }
-            continue
-        }
+        });
+
     }
 }
 
