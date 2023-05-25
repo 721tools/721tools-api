3b4931f54b35ad56a6d3954d183028f8130ac1fd
veryyoung <veryyoung.me@gmail.com>
2023-05-25 20:06:08 +0900

    Refactor opensea limit buy

--- src/tasks/opensea-limit-order-buy.ts
+++ src/tasks/opensea-limit-order-buy.ts
@@ -15,143 +15,145 @@ import { getContractWethAllowance, getWethBalance } from '../helpers/opensea/erc
 
 import { redis } from '../dal/mq';
 
+const provider = new ethers.providers.JsonRpcProvider(process.env.NETWORK === 'goerli' ? process.env.GOERLI_RPC_URL : process.env.ETH_RPC_URL);
 
 async function main(): Promise<void> {
     const sub = redis.duplicate();
     await sub.connect();
-    const provider = new ethers.providers.JsonRpcProvider(process.env.NETWORK === 'goerli' ? process.env.GOERLI_RPC_URL : process.env.ETH_RPC_URL);
     await sub.subscribe("OPENSEA-ETH-ORDER-LISTING", async (str) => {
-        const message = JSON.parse(str);
-        if (message.payload.payload.item.chain.name !== "ethereum" && message.payload.payload.item.chain.name !== process.env.NETWORK) {
-            return;
-        }
+        await handleMessage(str);
+    });
+}
 
-        const tokenSymbol = message.payload.payload.payment_token.Symbol;
-        if (tokenSymbol !== "ETH") {
-            return;
-        }
+const handleMessage = async (str) => {
+    const message = JSON.parse(str);
+    if (message.payload.payload.item.chain.name !== "ethereum" && message.payload.payload.item.chain.name !== process.env.NETWORK) {
+        return;
+    }
 
-        // example ethereum/0xded87c5e52c2ddc1934c10e751a1756c4f99ca98/85
-        const contractAddress = message.payload.payload.item.nft_id.split("/")[1];
-        const tokenId = message.payload.payload.item.nft_id.split("/")[2]
-        const price = parseFloat(ethers.utils.formatUnits(message.payload.payload.base_price, 'ether'));
-        const collection_slug = message.payload.payload.collection ? message.payload.payload.collection.slug : '';
-        const owner_address = message.payload.payload.maker.address;
-        const order_created_date = message.payload.payload.listing_date;
-        const order_expiration_date = message.payload.payload.expiration_date;
-        const order_event_timestamp = message.payload.payload.event_timestamp;
-        const order_sent_at = message.payload.sent_at;
+    const tokenSymbol = message.payload.payload.payment_token.Symbol;
+    if (tokenSymbol !== "ETH") {
+        return;
+    }
 
-        const limitOrders = await LimitOrders.findAll({
-            where: {
-                status: {
-                    [Sequelize.Op.in]: [LimitOrderStatus[LimitOrderStatus.INIT], LimitOrderStatus[LimitOrderStatus.RUNNING]]
-                },
-                expiration_time: {
-                    [Sequelize.Op.gt]: new Date()
-                },
-                price: {
-                    [Sequelize.Op.gte]: price
-                },
-                amount: { [Sequelize.Op.gt]: Sequelize.col('purchased') },
-                contract_address: contractAddress,
+    // example ethereum/0xded87c5e52c2ddc1934c10e751a1756c4f99ca98/85
+    const contractAddress = message.payload.payload.item.nft_id.split("/")[1];
+    const tokenId = message.payload.payload.item.nft_id.split("/")[2]
+    const price = parseFloat(ethers.utils.formatUnits(message.payload.payload.base_price, 'ether'));
+    const collection_slug = message.payload.payload.collection ? message.payload.payload.collection.slug : '';
+    const owner_address = message.payload.payload.maker.address;
+    const order_created_date = message.payload.payload.listing_date;
+    const order_expiration_date = message.payload.payload.expiration_date;
+    const order_event_timestamp = message.payload.payload.event_timestamp;
+    const order_sent_at = message.payload.sent_at;
 
+    const limitOrders = await LimitOrders.findAll({
+        where: {
+            status: {
+                [Sequelize.Op.in]: [LimitOrderStatus[LimitOrderStatus.INIT], LimitOrderStatus[LimitOrderStatus.RUNNING]]
             },
-            order: [['id', 'ASC']]
-        });
+            expiration_time: {
+                [Sequelize.Op.gt]: new Date()
+            },
+            price: {
+                [Sequelize.Op.gte]: price
+            },
+            amount: { [Sequelize.Op.gt]: Sequelize.col('purchased') },
+            contract_address: contractAddress,
 
-        for (const limitOrder of limitOrders) {
-            if (limitOrder.contract_address !== contractAddress) {
-                continue;
-            }
+        },
+        order: [['id', 'ASC']]
+    });
 
-            const pendingCount = await OrderBuyLogs.count({
-                order_id: limitOrder.id,
-                status: BuyStatus[BuyStatus.RUNNING]
-            });
-
-            if (pendingCount + limitOrder.purchased >= limitOrder.amount) {
-                continue;
-            }
-
-            const user = await User.findOne({
-                where: {
-                    id: limitOrder.user_id
-                }
-            });
-
-            if (user.valid == 0) {
-                continue;
-            }
-
-            if (user.type !== UserType[UserType.LIFELONG] && user.expiration_time < new Date()) {
-                continue;
-            }
-            if (ethers.utils.getAddress(user.address) == ethers.utils.getAddress(owner_address)) {
-                continue;
-            }
-
-            const collection = await OpenseaCollections.findOne({
-                where: {
-                    contract_address: parseAddress(limitOrder.contract_address)
-                }
-            });
-            if (!collection) {
-                continue;
-            }
-            if (collection.status == 1) {
-                continue;
-            }
-
-            const tokenIds = limitOrder.token_ids;
-            if (null != tokenIds && tokenIds.length > 0) {
-                if (!tokenIds.includes(tokenId)) {
-                    continue;
-                }
-            }
-            if (limitOrder.skip_flagged || !_.isEmpty(limitOrder.traits)) {
-                const where = limitOrder.skip_flagged ? {
-                    contract_address: collection.contract_address,
-                    supports_wyvern: true,
-                    token_id: parseTokenId(tokenId)
-                } : {
-                    contract_address: collection.contract_address,
-                    token_id: parseTokenId(tokenId),
-                };
-
-                const item = await OpenseaItems.findOne({
-                    where: where
-                });
-                if (!item) {
-                    continue;
-                }
-                if (!traitsMatched(item.traits, limitOrder.traits)) {
-                    continue;
-                }
-            }
-
-
-
-            const wethBalance = parseFloat(ethers.utils.formatEther(await getWethBalance(provider, user.address)));
-            if (wethBalance < price) {
-                continue;
-            }
-
-            const wethAllowance = parseFloat(ethers.utils.formatEther(await getContractWethAllowance(provider, process.env.CONTRACT_ADDRESS, user.address)));
-            if (wethAllowance < price) {
-                continue;
-            }
-            await buy(provider, user, limitOrder, contractAddress, [{
-                platform: Flatform.OPENSEA,
-                token_id: tokenId,
-                price: price,
-            }], "");
+    for (const limitOrder of limitOrders) {
+        if (limitOrder.contract_address !== contractAddress) {
             continue;
         }
 
+        const pendingCount = await OrderBuyLogs.count({
+            order_id: limitOrder.id,
+            status: BuyStatus[BuyStatus.RUNNING]
+        });
+
+        if (pendingCount + limitOrder.purchased >= limitOrder.amount) {
+            continue;
+        }
+
+        const user = await User.findOne({
+            where: {
+                id: limitOrder.user_id
+            }
+        });
+
+        if (user.valid == 0) {
+            continue;
+        }
+
+        if (user.type !== UserType[UserType.LIFELONG] && user.expiration_time < new Date()) {
+            continue;
+        }
+        if (ethers.utils.getAddress(user.address) == ethers.utils.getAddress(owner_address)) {
+            continue;
+        }
+
+        const collection = await OpenseaCollections.findOne({
+            where: {
+                contract_address: parseAddress(limitOrder.contract_address)
+            }
+        });
+
+        if (!collection) {
+            continue;
+        }
+        if (collection.status == 1) {
+            continue;
+        }
+
+        const tokenIds = limitOrder.token_ids;
+        if (null != tokenIds && tokenIds.length > 0) {
+            if (!tokenIds.includes(tokenId)) {
+                continue;
+            }
+        }
+        if (limitOrder.skip_flagged || !_.isEmpty(limitOrder.traits)) {
+            const where = limitOrder.skip_flagged ? {
+                contract_address: collection.contract_address,
+                supports_wyvern: true,
+                token_id: parseTokenId(tokenId)
+            } : {
+                contract_address: collection.contract_address,
+                token_id: parseTokenId(tokenId),
+            };
+
+            const item = await OpenseaItems.findOne({
+                where: where
+            });
+            if (!item) {
+                continue;
+            }
+            if (!traitsMatched(item.traits, limitOrder.traits)) {
+                continue;
+            }
+        }
 
 
-    });
+
+        const wethBalance = parseFloat(ethers.utils.formatEther(await getWethBalance(provider, user.address)));
+        if (wethBalance < price) {
+            continue;
+        }
+
+        const wethAllowance = parseFloat(ethers.utils.formatEther(await getContractWethAllowance(provider, process.env.CONTRACT_ADDRESS, user.address)));
+        if (wethAllowance < price) {
+            continue;
+        }
+        await buy(provider, user, limitOrder, contractAddress, [{
+            platform: Flatform.OPENSEA,
+            token_id: tokenId,
+            price: price,
+        }], "");
+        continue;
+    }
 }
 
 
